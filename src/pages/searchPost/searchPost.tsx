35ba946b69dd1db63f7be10efae918e87706c3a6
guziiuchyk <guziiuchyk@gmail.com>
2024-10-17 00:46:59 +0300

    add searchPost.tsx pagination

--- src/pages/searchPost/searchPost.tsx
+++ src/pages/searchPost/searchPost.tsx
@@ -10,25 +10,52 @@ import {GetApiPaginationGeneric, PostType} from "../../types/types.ts";
 
 const SearchPost: React.FC = () => {
 
-    const [posts, setPosts] = useState<PostType[] | null>(null);
+    const [posts, setPosts] = useState<PostType[]>([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalCount, setTotalCount] = useState(0);
+    const [fetching, setFetching] = useState(true);
 
     const searchText = useSelector((state: RootState) => state.search.value);
 
     useEffect(() => {
-        const fetchPosts = () => {
-            setIsLoading(true);
-            axios.get<GetApiPaginationGeneric<PostType>>(`http://localhost:8000/api/posts/${searchText}`).then((res) => {
-                setPosts(res.data.items);
-                setIsLoading(false);
-            }).catch((err) => {
-                console.log(err)
-                setIsLoading(false);
-            })
-        }
+        const fetchPosts = (): void => {
+            axios.get<GetApiPaginationGeneric<PostType>>(`http://localhost:8000/api/posts/${searchText}?size=5&page=${currentPage}`, {withCredentials: true})
+                .then((res) => {
+                    if (currentPage === 1) {
+                        setPosts(res.data.items);
+                    } else {
+                        setPosts((prevPosts) => [...prevPosts, ...res.data.items]);
+                    }
+                    setTotalCount(res.data.total);
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                    setFetching(false);
+                });
+        };
 
-        fetchPosts();
-    }, [searchText])
+        if (fetching) {
+            fetchPosts();
+        }
+    }, [fetching, currentPage, searchText]);
+
+    useEffect(() => {
+        const handleScroll = (e: Event) => {
+            const target = e.target as Document;
+            const scrollPosition = target.documentElement.scrollHeight - (target.documentElement.scrollTop + window.innerHeight);
+
+            if (scrollPosition < 200 && posts.length < totalCount && !fetching) {
+                setFetching(true);
+                setCurrentPage((prevPage) => prevPage + 1);
+            }
+        };
+
+        document.addEventListener("scroll", handleScroll);
+        return () => {
+            document.removeEventListener("scroll", handleScroll);
+        };
+    }, [posts, totalCount, fetching]);
 
     return (
         <>
